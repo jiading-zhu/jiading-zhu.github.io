d39b21c19846e3dd3b6315f694da42390d6704eb
jiading-zhu <jiading.light.zhu@gmail.com>
2026-07-01 23:53:14 -0400

    Add sitemap and robots discovery

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://jiading-zhu.github.io",
   output: "static",
+  integrations: [sitemap()],
 });
-
